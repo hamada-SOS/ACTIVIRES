5277bd712f5a1a037f86c7d60cfcdc1dc13def3a
Hamada <85646705+hamada-SOS@users.noreply.github.com>
2023-05-08 15:02:50 +0300

    commit

--- app/src/main/java/com/example/actiandintprac/MainActivity.kt
+++ app/src/main/java/com/example/actiandintprac/MainActivity.kt
@@ -5,30 +5,45 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.actiandintprac.adapter.CardAdapter
 import com.example.actiandintprac.databinding.ActivityMainBinding
 import com.example.actiandintprac.model.Card
+import com.example.actiandintprac.model.Datasource
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity(), CardAdapter.RecyclerViewEvent{
 
 
     @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        binding.recyclerView1.adapter = CardAdapter(this)
-        binding.recyclerView1.layoutManager =
+        val recyclerView1: RecyclerView = findViewById(R.id.recyclerView1)
+        val recyclerView2: RecyclerView = findViewById(R.id.recyclerView2)
+
+
+        val myaa = CardAdapter(this, this)
+        recyclerView1.adapter = myaa
+        recyclerView1.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
-        binding.recyclerView2.adapter = CardAdapter(this)
-        binding.recyclerView2.layoutManager =
+        recyclerView2.adapter = myaa
+        recyclerView2.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-
     }
+
+
+    override fun onItemClick(position: Int) {
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent )
+    }
+
+
+
+
+
 }
--- app/src/main/java/com/example/actiandintprac/adapter/CardAdapter.kt
+++ app/src/main/java/com/example/actiandintprac/adapter/CardAdapter.kt
@@ -1,33 +1,44 @@
 package com.example.actiandintprac.adapter
 
 import android.content.Context
-import android.graphics.ColorSpace
 import android.view.LayoutInflater
-import android.view.View.OnClickListener
+import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.actiandintprac.databinding.CardItemsBinding
+import com.example.actiandintprac.R
 import com.example.actiandintprac.model.Datasource
-import com.example.actiandintprac.model.Card as Card
 
-class CardAdapter(context: Context) : RecyclerView.Adapter<CardAdapter.CardViewHolder>() {
+class CardAdapter(context: Context, private val listener: RecyclerViewEvent) :
+    RecyclerView.Adapter<CardAdapter.CardViewHolder>() {
     val dataset = Datasource.cards
 
-    var onItemClick : ((Card) -> Unit)? = null
+
+    inner class CardViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+
+        init {
+            view.setOnClickListener(this)
+        }
+
+        override fun onClick(p0: View?) {
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onItemClick(position)
+            }
+        }
+
+        val cardName: TextView = view.findViewById(R.id.cardName)
+        val cardPrice: TextView = view.findViewById(R.id.cardPrice)
+        val cardImage: ImageView = view.findViewById(R.id.cardImage)
 
 
-
-
-
-
-    class CardViewHolder(val binding: CardItemsBinding) : RecyclerView.ViewHolder(binding.root){
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
-        val adapterlayout =
-            CardItemsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return CardViewHolder(adapterlayout)
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.card_items, parent, false)
+        return CardViewHolder(view)
     }
 
     override fun getItemCount(): Int {
@@ -36,19 +47,16 @@ class CardAdapter(context: Context) : RecyclerView.Adapter<CardAdapter.CardViewH
 
     override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
         val item = dataset[position]
-        holder.binding.cardName.text = item.cardname
-        holder.binding.cardPrice.text = item.price
-        holder.binding.cardImage.setImageResource(item.image)
-        holder.itemView.setOnClickListener {
-            onItemClick?.invoke(item)
-        }
-
+        holder.cardName.text = item.cardname
+        holder.cardPrice.text = item.price
+        holder.cardImage.setImageResource(item.image)
 
 
     }
 
-
-
+    interface RecyclerViewEvent {
+        fun onItemClick(position: Int)
+    }
 
 }
 
